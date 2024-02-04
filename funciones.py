5c65b9aecf66eb5eb07bf358b8e06bd8a9eba48b
Juan Munizaga <juanmuni@espol.edu.ec>
2024-02-03 20:05:55 -0500

    Implementacion de interfaz grafica

--- funciones.py
+++ funciones.py
@@ -1,43 +1,4 @@
 import sympy as sp
-from sympy import init_printing
-init_printing() 
-
-# calcular integral con a = 2
-def calcularIntegral(a):
-    pi = sp.pi
-    t= sp.symbols('t')
-    dx_dt = sp.diff(a*sp.cos(t), t)
-    dy_dt = sp.diff(a*sp.sin(t), t)
-    dz_dt = sp.diff(4-(a/2)*(sp.cos(t)+sp.sin(t)), t)
-    expr = sp.sqrt(dx_dt**2 + dy_dt**2 + dz_dt**2)
-    return sp.integrate(expr,(t,0,2*pi))
-
-def calcularTraza(a, n):
-    #declaracion de variables 'symbols'
-    pi = sp.pi
-    t= sp.symbols('t')
-    #parametrizacion de la curva
-    expr_x = a * sp.cos(t)
-    expr_y = a * sp.sin(t)
-    expr_z = 4 - (a/2)*(sp.cos(t)+sp.sin(t))
-    #derivadas con respecto a "t"
-    dx_dt = sp.diff(expr_x, t)
-    dy_dt = sp.diff(expr_y, t)
-    dz_dt = sp.diff(expr_z, t)
-    #expresion longitud de arco
-    expr = sp.sqrt(dx_dt**2 + dy_dt**2 + dz_dt**2)
-    resultado = 0
-    #incremento
-    inter = (2*pi)/n
-    t_i = 0
-    #punto derecho
-    for i in range(1,n+1):
-        resultado += expr.subs(t, t_i + (inter*i))
-        t_i += inter
-    longAprox = resultado*inter
-    return longAprox.evalf()
-
-
 
 def trazaConSum(a,n):
     #declaracion de variables 'symbols'
@@ -68,10 +29,3 @@ def trazaConSum(a,n):
     """
     sumt = sp.Sum(f,(i,1,n))
     return (sumt.evalf()*delta_t) 
-
-a = 100
-n = 1000
-#resultado = calcularTraza(a, n)
-#print(f"Resultado de la traza: {resultado}")
-resultado2 = trazaConSum(a,n)
-print(resultado2)
--- grafica.py
+++ grafica.py
@@ -1,21 +1,79 @@
+import tkinter
+from tkinter import simpledialog
+from matplotlib.animation import FuncAnimation
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from funciones import trazaConSum
+# Crear una ventana emergente para solicitar el valor de A
+root = tkinter.Tk()
+root.withdraw()  # Ocultar la ventana principal
 
-fig = plt.figure(figsize = (10,8))
-ax = fig.add_subplot(111, projection='3d')
-theta = np.arange(0,2*np.pi,0.01)
-a = 5
-z = np.arange(0,100,10)
-Theta, z_ = np.meshgrid(theta,z)
-# x, y en polares
-x = a * np.cos(Theta)
-y = a * np.sin(Theta)
-ax.plot_surface(x,y,z_,color='Pink')
-def plano(a):
-    t = np.linspace(0,2*np.pi,100)
-    x = a * np.cos(t)
-    y = a * np.sin(t)
-    z = 4 - (a/2)*(np.cos(t)+np.sin(t))
-    ax.plot(x,y,z)
-plano(5)
-plt.show()
+# Solicitar al usuario que ingrese un valor
+a = simpledialog.askfloat("Valor de A", "Ingrese el valor de A:")
+
+# Solicitar al usuario que ingrese un valor
+n = simpledialog.askinteger("Valor de N", "Cuantos sub-intervalos:")
+
+# Cerrar la ventana emergente
+root.destroy()
+
+# Validar que se haya ingresado un valor
+if a is not None and n is not None:
+    #print(funciones.trazaConSum(a,n))
+    valorTraza = trazaConSum(a,n)
+    fig = plt.figure(figsize=(12,8))
+    ax = fig.add_subplot(111, projection='3d')
+
+    def grafCilindro(a):
+        # Coordenadas esféricas
+        theta_ = np.linspace(0, 2*np.pi, 100)
+        z_ = np.linspace(-10*a,10*a,100)
+        theta, z_cili = np.meshgrid(theta_,z_)
+        rho = a  # Radio del cilindro
+
+        # Coordenadas cartesianas
+        x_cili = rho * np.cos(theta)
+        y_cili = rho * np.sin(theta)
+
+        # Graficar el cilindro
+        ax.plot_surface(x_cili,y_cili,z_cili, color='bisque',zorder=0,alpha=0.35)
+
+    def grafPlano(a):
+        def z(x,y):
+            return (8-x-y)/2
+        x_plano = np.linspace(-10*a/2,10*a/2)
+        y_plano = np.linspace(-10*a/2,10*a/2)
+        x_plano,y_plano = np.meshgrid(x_plano,y_plano)
+        z_plano = z(x_plano,y_plano)
+        ax.plot_surface(x_plano,y_plano,z_plano,color='pink',zorder=2,alpha=0.85)
+
+    def traza(a):
+        t = np.linspace(0,2*np.pi)
+        expr_x = a*np.cos(t)
+        expr_y = a*np.sin(t)
+        expr_z = 4 - (1/2)*(expr_x+expr_y)
+        ax.plot(expr_x,expr_y,expr_z,color='black',zorder=1)
+
+    grafCilindro(a)
+    grafPlano(a)
+    traza(a)
+    # Agregar texto dentro de la ventana de Matplotlib
+    ax.text2D(0.05, 0.95, f"El valor de la traza es: {valorTraza}", transform=ax.transAxes,ha='center', va='center', fontsize=16, bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.6))
+    #giro
+    def update(num, ax, a):
+        ax.view_init(elev=num, azim=num*2)  # Cambiar la elevación y el azimut para rotar en Z
+
+    ani = FuncAnimation(fig, update, frames=np.arange(0, 360, 2), fargs=(ax, a), interval=50)
+
+    # Configurar el aspecto del gráfico
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_title(f'Calculo Traza $x^2 + y^2 = {a}^2$ y x+y+2z=8')
+    # Ajustar automáticamente la escala de los ejes
+    ax.auto_scale_xyz([-a*5, a*5], [-a*5, a*5], [0, a*5])
+    # Rotar el gráfico para una mejor visualización inicial
+    ax.legend()
+
+    plt.show()
